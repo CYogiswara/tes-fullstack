193883791724ce2af59c47ccdc61ae3cf73ee581
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2025-11-09 19:40:30 +0700

    Cart function  (frontend)
    Clicking the "Add to Cart" button will send the menu to localStorage "cart". If the menu clicked is already in the "cart" local storage, then add the quantity by 1 (using the menu's id as the reference), if not then add it to the localstorage.

--- tes-fullstack/frontend/vite-react-fullstack/src/pages/homePage.jsx
+++ tes-fullstack/frontend/vite-react-fullstack/src/pages/homePage.jsx
@@ -5,7 +5,10 @@ import Navbar from "../components/navbar";
 function HomePage() {
   const [menus, setMenu] = useState([]);
   const [user, setUser] = useState(null)
+  const [cart, setCart] = useState([])
+
   const navigate = useNavigate()
+
 
   useEffect(() => {
     fetch("http://localhost:3000/menu")
@@ -17,10 +20,49 @@ function HomePage() {
       .catch(err => console.error("Fetch error:", err));
 
       const savedUser = localStorage.getItem("user")
+      const savedCart = localStorage.getItem("cart")
       if(savedUser){
         setUser(JSON.parse(savedUser))
       }
+
+      if(savedCart){
+        setCart(JSON.parse(savedCart))
+      }
   }, [])
+
+  function addToCart(menu) {
+    //GET THE EXISTING CART FIRST:
+    const existingCart = JSON.parse(localStorage.getItem("cart")) || []
+    if (existingCart && existingCart !== "undefined" && existingCart !== "null") {
+      try {
+        setCart(JSON.parse(existingCart));
+      } catch (err) {
+        console.error("Invalid cart data:", err);
+        setCart([]);
+      }
+    }
+
+    //FIND IF THE MENU CLICKED IS ALREADY IN CART:
+    const menuIsInCart = existingCart.find((item) => item.id_menu === menu.id_menu)
+
+    //USE menuIsInCart:
+    if(menuIsInCart){
+      menuIsInCart.quantity += 1
+    }else{
+      existingCart.push({
+        id_menu: menu.id_menu,
+        nama_menu: menu.nama_menu,
+        quantity: 1,
+        harga: menu.harga
+      })
+    }
+
+    //ADD TO LOCALSTORAGE:
+    localStorage.setItem("cart", JSON.stringify(existingCart))
+    setCart(existingCart)
+
+    alert(`${menu.nama_menu} added to cart`)
+  }
 
 
   return (
@@ -31,11 +73,11 @@ function HomePage() {
       <table border="1" cellPadding="8" style={{ marginTop: "20px" }}>
         <tbody>
           {menus.map(menu => (
-            <tr key={menu.id}>
+            <tr key={menu.id_menu}>
               <td>{menu.nama_menu}</td>
               <td>{menu.harga}</td>
               <td>{menu.available}</td>
-              <td><button>Add to Cart</button></td>
+              <td><button onClick={() => addToCart(menu)}>Add to Cart</button></td>
             </tr>
           ))}
         </tbody>
